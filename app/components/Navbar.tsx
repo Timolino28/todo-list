4c6ed0e8eb14f2a216e0321ca3c6bbef62b7cc67
Timolino28 <kordmann@web.de>
2025-10-07 20:44:02 +0200

    Adding navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import { Button } from "./ui/button";
+import { Link } from "react-router";
+
+function navbar() {
+  const links = [
+    { name: "Pricing", link: "/pricing" },
+    { name: "FAQ", link: "/faq" },
+    { name: "Reviews", link: "/reviews" },
+  ];
+
+  return (
+    <div className="fixed top-0 left-0 w-full flex justify-between items-center p-5 border-b-[0.5px] border-gray-300/20">
+      <Link to={"/"}>
+        <div className="text-xl text-gray-300 font-bold">Mach!</div>
+      </Link>
+
+      <div>
+        <ul className="flex gap-10">
+          {links.map((link) => (
+            <li className=" text-gray-300 text-[14px] md:text-[16px]">
+              <a
+                href={link.link}
+                className="hover:border-b-1 hover:border-gray-300"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Link to={"/login"}>
+          <Button className="cursor-pointer bg-neutral-700 hover:bg-neutral-700 border-1 border-gray-300/20 hover:border-gray-300/50">
+            Login
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -11,13 +11,6 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <>
-      <div className="text-gray-300">
-        Hallo, Du Ferkel
-        <div>
-          <Button className="bg-oranje">Click Me</Button>
-        </div>
-      </div>
-      {/* Erstmal noch NavBar bauen -> die machen wir handmade */}
       {/* Hier kommt die Landingpage hin - übe das ein bisschen hinsichtlich Design etc. */}
     </>
   );

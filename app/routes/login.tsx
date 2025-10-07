3df912d9131b08afebfc3fdd7d2a883643a99599
Timolino28 <kordmann@web.de>
2025-10-07 21:14:42 +0200

    Finishing login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "~/components/login-form";
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm">
+      <div className="bg-neutral-700/30 rounded-xl p-10 max-w-sm border-[0.5px] border-gray-300/20">
         <LoginForm />
       </div>
     </div>

ae4898a8e93f077ccb33da73cdeedd92d0ec9b50
Timolino28 <kordmann@web.de>
2025-10-12 11:46:16 +0200

    Adjusting Calendar component

--- app/components/app/Calendar.tsx
+++ app/components/app/Calendar.tsx
@@ -3,14 +3,11 @@ import { getCurrentWeek, type WeekDay } from "~/utils/getCurrentWeek";
 
 function Calendar() {
   const week: WeekDay[] = getCurrentWeek();
-  const today = new Date();
 
   return (
     <>
       <div className="sm:grid grid-cols-7 gap-5 flex flex-col p-10">
         {week.map((day) => {
-          const isToday = day.date.toDateString() === today.toDateString();
-
           return <WeekDays key={day.formatted} day={day} />;
         })}
       </div>
